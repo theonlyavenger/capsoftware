bf3ea9db3df6fd657b208071d007a44cff61d24d
Manoj <manojjahgirdar@gmail.com>
2018-07-25 15:32:47 +0530

    Analytics bar graph added

---
+++ analytics.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace analytics
+{
+    partial class analytics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.headerpannel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.analyticsChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.headerpannel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.analyticsChart)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // headerpannel
+            // 
+            this.headerpannel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.headerpannel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.headerpannel.Controls.Add(this.label1);
+            this.headerpannel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerpannel.Location = new System.Drawing.Point(0, 0);
+            this.headerpannel.Name = "headerpannel";
+            this.headerpannel.Size = new System.Drawing.Size(910, 58);
+            this.headerpannel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COMPUTRONICS - ANALYTICS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(411, 393);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 20);
+            this.label7.TabIndex = 124;
+            this.label7.Text = "COURSES";
+            // 
+            // analyticsChart
+            // 
+            this.analyticsChart.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.analyticsChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.analyticsChart.Legends.Add(legend1);
+            this.analyticsChart.Location = new System.Drawing.Point(82, 6);
+            this.analyticsChart.Name = "analyticsChart";
+            this.analyticsChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.DashedVertical;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "COURSE";
+            this.analyticsChart.Series.Add(series1);
+            this.analyticsChart.Size = new System.Drawing.Size(816, 384);
+            this.analyticsChart.TabIndex = 125;
+            this.analyticsChart.Text = "chart1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.analyticsChart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(910, 613);
+            this.panel1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 124;
+            this.label4.Text = "COUNT";
+            // 
+            // analytics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 671);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.headerpannel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "analytics";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.analytics_Load);
+            this.headerpannel.ResumeLayout(false);
+            this.headerpannel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.analyticsChart)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headerpannel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart analyticsChart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ analytics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace analytics
+{
+    public partial class analytics : Form
+    {
+        public analytics()
+        {
+            InitializeComponent();
+        }
+
+        private void analytics_Load(object sender, EventArgs e)
+        {
+            analyticsChart.Series["COURSE"].Points.AddXY("C", 40);
+            analyticsChart.Series["COURSE"].Points.AddXY("C++", 10);
+            analyticsChart.Series["COURSE"].Points.AddXY("C#", 5);
+            analyticsChart.Series["COURSE"].Points.AddXY("JAVA", 45);
+            analyticsChart.Series["COURSE"].Points.AddXY("PYTHON", 20);
+
+        }
+    }
+}
