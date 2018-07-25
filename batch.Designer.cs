521f1663c5b5dbc99dd91ff74e8012fca285fc77
radhika <radhikapatil226@gmail.com>
2018-07-25 16:41:14 +0530

    batch

---
+++ batch.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace batches
+{
+    partial class batch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerpannel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnshow = new System.Windows.Forms.Button();
+            this.cbbatchlist = new System.Windows.Forms.ComboBox();
+            this.clbdisplaylist = new System.Windows.Forms.CheckedListBox();
+            this.headerpannel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // headerpannel
+            // 
+            this.headerpannel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.headerpannel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.headerpannel.Controls.Add(this.label1);
+            this.headerpannel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerpannel.Location = new System.Drawing.Point(0, 0);
+            this.headerpannel.Name = "headerpannel";
+            this.headerpannel.Size = new System.Drawing.Size(910, 58);
+            this.headerpannel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COMPUTRONICS - BATCH";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.clbdisplaylist);
+            this.panel1.Controls.Add(this.cbbatchlist);
+            this.panel1.Controls.Add(this.btnshow);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(910, 613);
+            this.panel1.TabIndex = 3;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(81, 116);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 20);
+            this.label10.TabIndex = 124;
+            this.label10.Text = "Value";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(81, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 20);
+            this.label9.TabIndex = 124;
+            this.label9.Text = "Value";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(81, 51);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 20);
+            this.label8.TabIndex = 124;
+            this.label8.Text = "Value";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(81, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 20);
+            this.label7.TabIndex = 124;
+            this.label7.Text = "Value";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 20);
+            this.label6.TabIndex = 123;
+            this.label6.Text = "Label :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 20);
+            this.label5.TabIndex = 123;
+            this.label5.Text = "Label :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 20);
+            this.label4.TabIndex = 123;
+            this.label4.Text = "Label :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 123;
+            this.label3.Text = "Label :";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(0, 176);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(910, 35);
+            this.panel2.TabIndex = 122;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 20);
+            this.label2.TabIndex = 100;
+            this.label2.Text = "HEADER IF REQUIRED";
+            // 
+            // btnshow
+            // 
+            this.btnshow.Location = new System.Drawing.Point(646, 460);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(75, 23);
+            this.btnshow.TabIndex = 126;
+            this.btnshow.Text = "Show";
+            this.btnshow.UseVisualStyleBackColor = true;
+            this.btnshow.Click += new System.EventHandler(this.btnshow_Click);
+            // 
+            // cbbatchlist
+            // 
+            this.cbbatchlist.FormattingEnabled = true;
+            this.cbbatchlist.Location = new System.Drawing.Point(630, 285);
+            this.cbbatchlist.Name = "cbbatchlist";
+            this.cbbatchlist.Size = new System.Drawing.Size(151, 21);
+            this.cbbatchlist.TabIndex = 127;
+            this.cbbatchlist.SelectedIndexChanged += new System.EventHandler(this.cbbatchlist_SelectedIndexChanged);
+            // 
+            // clbdisplaylist
+            // 
+            this.clbdisplaylist.FormattingEnabled = true;
+            this.clbdisplaylist.Location = new System.Drawing.Point(66, 291);
+            this.clbdisplaylist.Name = "clbdisplaylist";
+            this.clbdisplaylist.Size = new System.Drawing.Size(299, 364);
+            this.clbdisplaylist.TabIndex = 128;
+            // 
+            // batch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 671);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.headerpannel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "batch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.batch_Load);
+            this.headerpannel.ResumeLayout(false);
+            this.headerpannel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headerpannel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnshow;
+        private System.Windows.Forms.ComboBox cbbatchlist;
+        private System.Windows.Forms.CheckedListBox clbdisplaylist;
+    }
+}
+
---
+++ batch.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
+namespace batches
+{
+    public partial class batch : Form
+    {
+        private MySqlConnection con;
+        string server;
+        string database;
+        string uid;
+        string pwd;
+        public batch()
+        {
+            InitializeComponent();
+        }
+
+        private void batch_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            
+            
+            InitializeDb();
+            displayCourse();
+            cbbatchlist.SelectedIndex = 0;
+          
+        }
+        private void InitializeDb()
+        {
+            server = "localhost";
+            database = "computronics_admission";
+            uid = "root";
+            pwd = " ";
+
+            string constr = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + pwd + ";";
+            con = new MySqlConnection(constr);
+
+        }
+        private void displayData()
+        {
+            try
+            {
+                con.Open();
+              //  string selected = this.cbbatchlist.GetItemText(this.cbbatchlist.SelectedItem);
+               // MessageBox.Show(selected);
+      
+                string query = "SELECT stud_id,stud_name FROM student where stud_courseSelected='"+cbbatchlist.Text +"'";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    clbdisplaylist.Items.Add(reader["stud_id"].ToString() + "   " + reader["stud_name"].ToString());//fetching & adding id & name to checkedlistbox
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+
+        private void displayCourse()
+        {
+            try
+            {
+                con.Open();
+                string query = "SELECT distinct course_name FROM course ";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    cbbatchlist.Items.Add(reader["course_name"].ToString());//fetching & adding id & name to checkedlistbox
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+      /*  private void makeBatch()
+        {
+            ArrayList batch = new ArrayList();              
+                con.Open();
+                string query = "SELECT distinct course_name FROM course";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    batch.Add(reader["course_name"].ToString());
+                    //todo cmb box
+                }
+
+           // cmb chageevent 
+
+            foreach(string name in batch)
+            {
+                string q = "SELECT stud_id,stud_name,stud_courseSelected, FROM student where stud_courseSelected='"+name+"' ";
+                MySqlCommand comm = new MySqlCommand(q, con);
+                MySqlDataReader read = cmd.ExecuteReader();
+
+
+
+                try
+                {
+                   
+                    while (read.Read())
+                    {
+                       // cbbatchlist.items.Add(read["course_name"].ToString());
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Message not emailed: " + ex.Message.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                    read.Close();
+                }
+            }
+        }*/
+    
+
+        private void btnshow_Click(object sender, EventArgs e)
+        {
+           // makeBatch();
+        }
+
+        private void cbbatchlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            clbdisplaylist.Items.Clear();
+            displayData();
+          
+        }
+       
+    }
+}
