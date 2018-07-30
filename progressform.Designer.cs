5578a3d6b125fa1185d6c453d3192a7a622a0fc7
Megha <shahapurkarmegha@gmail.com>
2018-07-30 13:34:05 +0530

    added progressform

---
+++ progressform.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace progressform2
+{
+    partial class progressform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerpannel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.centerpanel = new System.Windows.Forms.Panel();
+            this.bottompanel = new System.Windows.Forms.Panel();
+            this.dataTable = new System.Windows.Forms.DataGridView();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.headerpannel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.centerpanel.SuspendLayout();
+            this.bottompanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // headerpannel
+            // 
+            this.headerpannel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.headerpannel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.headerpannel.Controls.Add(this.label1);
+            this.headerpannel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerpannel.Location = new System.Drawing.Point(0, 0);
+            this.headerpannel.Name = "headerpannel";
+            this.headerpannel.Size = new System.Drawing.Size(910, 58);
+            this.headerpannel.TabIndex = 2;
+            this.headerpannel.Paint += new System.Windows.Forms.PaintEventHandler(this.headerpannel_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COMPUTRONICS-PROGRESS FORM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.centerpanel);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(910, 613);
+            this.panel1.TabIndex = 3;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.exitToolStripMenuItem.Text = "Edit";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(910, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // centerpanel
+            // 
+            this.centerpanel.Controls.Add(this.dataTable);
+            this.centerpanel.Controls.Add(this.bottompanel);
+            this.centerpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.centerpanel.Location = new System.Drawing.Point(0, 24);
+            this.centerpanel.Name = "centerpanel";
+            this.centerpanel.Size = new System.Drawing.Size(910, 589);
+            this.centerpanel.TabIndex = 2;
+            // 
+            // bottompanel
+            // 
+            this.bottompanel.Controls.Add(this.btnSubmit);
+            this.bottompanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottompanel.Location = new System.Drawing.Point(0, 544);
+            this.bottompanel.Name = "bottompanel";
+            this.bottompanel.Size = new System.Drawing.Size(910, 45);
+            this.bottompanel.TabIndex = 0;
+            // 
+            // dataTable
+            // 
+            this.dataTable.AllowUserToAddRows = false;
+            this.dataTable.AllowUserToDeleteRows = false;
+            this.dataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataTable.Location = new System.Drawing.Point(0, 0);
+            this.dataTable.Name = "dataTable";
+            this.dataTable.ReadOnly = true;
+            this.dataTable.Size = new System.Drawing.Size(910, 544);
+            this.dataTable.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnSubmit.Location = new System.Drawing.Point(418, 8);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 31);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "OK";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // progressform2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 671);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.headerpannel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "progressform2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.progressform2_Load);
+            this.headerpannel.ResumeLayout(false);
+            this.headerpannel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.centerpanel.ResumeLayout(false);
+            this.bottompanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headerpannel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel centerpanel;
+        private System.Windows.Forms.DataGridView dataTable;
+        private System.Windows.Forms.Panel bottompanel;
+        private System.Windows.Forms.Button btnSubmit;
+        // private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+    }
+}
+
---
+++ progressform.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace progressform2
+{
+    public partial class progressform : Form
+    {
+        private MySqlConnection con;
+        string server;
+        string database;
+        string uid;
+        string pwd;
+        DataTable dt;
+        public progressform()
+        {
+            InitializeComponent();
+        }
+
+        private void progressform2_Load(object sender, EventArgs e)
+        {
+
+            dt = new DataTable();
+            dataTable.DataSource = null;
+            InitilizeDb();
+            DisplayData();
+
+        }
+        private void InitilizeDb()
+        {
+            server = "localhost";
+            database = "computronics_admission_process";
+            uid = "root";
+            pwd = " ";
+
+            string constr = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + pwd + ";";
+            con = new MySqlConnection(constr);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void DisplayData()
+        {
+            // Adding column with their properties
+            dataTable.ColumnCount = 2;
+
+            dataTable.Columns[0].HeaderText = "BATCH";
+            dataTable.Columns[0].Name = "batch";
+
+            dataTable.Columns[1].HeaderText = "NAME";
+            dataTable.Columns[1].Name = "name";
+            dataTable.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            DataGridViewCheckBoxColumn chkexam = new DataGridViewCheckBoxColumn();
+            dataTable.Columns.Add(chkexam);
+            chkexam.HeaderText = "EXAM";
+            chkexam.Name = "chkexam";
+            chkexam.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            DataGridViewCheckBoxColumn chkcertificate = new DataGridViewCheckBoxColumn();
+            dataTable.Columns.Add(chkcertificate);
+            chkcertificate.HeaderText = "CERTIFICATE";
+            chkcertificate.Name = "chkcertificate";
+            chkcertificate.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+
+           /* dataTable.Columns[4].HeaderText = "DATE OF JOINING";
+            dataTable.Columns[4].Name = "doj";
+            dataTable.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            dataTable.Columns[5].HeaderText = "PAYMENT STATUS";
+             dataTable.Columns[5].Name = "payment";
+             dataTable.Columns[5].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill; */
+
+          
+          
+
+            string query = "SELECT batch_no,stud_name  from batch";
+            con.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(query, con);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                  
+                  
+                        string batch = reader["batch_no"].ToString();
+                        string name = reader["stud_name"].ToString();
+
+                        dataTable.Rows.Add(batch, name);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void headerpannel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+       
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+                   }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataTable_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
